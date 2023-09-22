bb144535c8448b60fde4b9b8b320e8eeaf2b9313
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-09-22 12:51:12 +0100

    JS code done for mathematical operations

---
+++ hw2/hw2.js
@@ -0,0 +1,21 @@
+function mathOperationsOnIntegers(num1, num2){
+    let additionResult = +num1 + (+num2);
+    let substructionResult = +num1 - (+num1);
+    let multiplicationResult = +num1 * (+num2);
+    let divisionResult = +num1/(+num2);
+
+    alert(`${num1} + ${num2} = ${additionResult}`);
+    alert(`${num1} - ${num2} = ${substructionResult}`);
+    alert(`${num1} * ${num2} = ${multiplicationResult}`);
+    alert(`${num1} / ${num2} = ${divisionResult}`);
+}
+
+let firstNumberRecieved = prompt("Please, enter any integer", 0);
+let secondNumberRecieved = prompt("Please, enter one more integer", 0);
+
+alert(`You have entered numbers ${firstNumberRecieved} and ${secondNumberRecieved}. Let's do some mathematical operations on them!`);
+
+
+mathOperationsOnIntegers(firstNumberRecieved, secondNumberRecieved);
+
+
