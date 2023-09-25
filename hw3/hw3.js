80c4db663098566f487a21c5eb76e7d44389928b
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-09-25 21:21:13 +0100

    If else operator to found out a finger of the hand

---
+++ hw3/hw3.js
@@ -0,0 +1,13 @@
+let fingerPointed = prompt("Please, enter the number of any finger of your hand considering thumb finger as number 1", 0);
+
+if (fingerPointed == 1){
+    alert("You have raised your thumb!");
+} else if(fingerPointed == 2){
+    alert("You have pointed with a pointer finger! Where is that you say?");
+} else if(fingerPointed == 3){
+    alert("I'm trying not to get you wrong but it's a middle finger..");
+} else if(fingerPointed == 4){
+    alert("I'll give you a hint - it's a ring finger! Chin-chin!");
+} else{
+    alert("Who's that little one? Oh, it's a pinky!");
+}
