cb2fe0215eeea3e6dc8633f9dacdc83d7d168f00
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-10-03 09:21:02 +0100

    Update hw4.js

--- hw4/hw4.js
+++ hw4/hw4.js
@@ -1,6 +1,6 @@
 let yearRecieved = prompt("Please, enter any year AD: ", 0000);
 
-let messageLeapYear = `Just to let you know, the ${yearRecieved} is a leap year`;
+let messageLeapYear = `Just to let you know, ${yearRecieved} is a leap year`;
 let messageYearNotLeap = `You have picked a year which is not leap`;
 
 yearRecieved % 400 == 0 || (yearRecieved % 4 == 0 && yearRecieved % 100 != 0) ? alert(messageLeapYear) : alert (messageYearNotLeap);
