d17315abf1dd978989854fe5138feb9e2d5c9c69
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-10-31 10:13:46 +0000

    Update hw10.js

--- hw10/hw10.js
+++ hw10/hw10.js
@@ -1,14 +1,15 @@
 /**Write a function-constructor ConstructCity which recieves city characteristics and creates objects of ConstructCity
  */
 
-function ConstructCity(name, country, footballClub){
+function ConstructCity(name, country, footballLeague, footballClub){
     this.name = name;
     this.country = country;
+    this.footballLeague = footballLeague;
     this.footballClub = footballClub;
 }
 
-let liverpool = new ConstructCity('liverpool', 'United Kingdom', 'Liverpool');
-let valencia = new ConstructCity('Valencia', 'Spain', 'Valencia');
+let liverpool = new ConstructCity('liverpool', 'United Kingdom', 'APL', 'Liverpool');
+let valencia = new ConstructCity('Valencia', 'Spain', 'La Liga', 'Valencia');
 
 console.log(liverpool);
 console.log(valencia);
