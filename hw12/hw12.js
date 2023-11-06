e8a7ded401f44b0c823870239bc8f5254a960680
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-11-06 11:31:54 +0000

    HW12 Palindrome Algorithm  String to Array and Back

---
+++ hw12/hw12.js
@@ -0,0 +1,18 @@
+/**
+ * Напишіть функцію palindrome(str). Поверніть true, якщо заданий рядок є паліндромом. В іншому випадку, поверніть false. Припустимо, що функція на вході отримує одну цілу строку без проміжків та лише з маленькими літерами латинської абетки та цифрами.
+ */
+
+function checkPalindrome(str){
+    let stringToArr = str.split('');
+    let arrReversed = stringToArr.reverse();
+    let arrBackToStr = arrReversed.join(''); 
+
+    if(str === arrBackToStr){
+        console.log('The string is a palindrome');
+    } else{
+        console.log('The string is not a palindrome');
+    }
+}
+
+let inputStr = prompt('Please, enter any string here to check');
+checkPalindrome(inputStr);
