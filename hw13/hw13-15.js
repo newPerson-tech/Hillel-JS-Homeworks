be29835bc892c864197807626ae8622664f0f4ed
Tetiana Paniot <tatyana.paniot@gmail.com>
2023-11-27 21:16:21 +0000

    Delete from local storage

--- hw13/hw13-15.js
+++ hw13/hw13-15.js
@@ -1,7 +1,5 @@
 /**
- * створіть масив рядків (наприклад, ["Пункт 1", "Пункт 2", "Пункт 3"]) або запитайте їх promptom
-створіть список (<ul>) та додайте кожен елемент масиву у вигляді окремого пункту списку
-додайте створений список до кінця <body>
+ * Create an array of values given from user via prompt, create a list element and set each array value as a list item
  */
 
 function renderListItem(task){
@@ -13,25 +11,36 @@ function renderListItem(task){
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'x';
     taskElement.append(deleteButton);
+
+    deleteButton.addEventListener('click', event => {
+        const listItemToDelete = event.target.previousElementSibling.innerHTML;
+        const tasks = localStorage.getItem('tasks').split(',');
+        const taskFiltered = tasks.filter(task => task !== listItemToDelete);
+        localStorage.setItem('tasks', taskFiltered);
+        event.target.closest('li').remove();
+    });
 }
 
 function init(){
     const appElement = document.getElementById('to-do-list-app');
-    const tasks = [];
+    let tasks = [];
 
-    while (true){
-        const task = prompt('What are you going to do today?', ' ');
-        if (task === null) break;
-        tasks.push(task);
-    }
-    
-    listElement = document.createElement('ul');
+    const listElement = document.createElement('ul');
     listElement.id = 'list';
     appElement.append(listElement); 
+
+    if(localStorage.getItem('tasks')){
+        tasks = localStorage.getItem('tasks').split(',');
+    } else{
+        while (true){
+            const task = prompt('What are you going to do today?', ' ');
+            if (task === null) break;
+            tasks.push(task);
+        }
+        localStorage.setItem('tasks', tasks);
+    }
     
-    tasks.forEach(task => {
-        renderListItem(task);
-    });
+    tasks.forEach(task => renderListItem(task));
     
     const addButton = document.createElement('button');
     addButton.innerHTML = '+';
@@ -39,6 +48,9 @@ function init(){
 
     addButton.addEventListener('click', () => {
         const task = prompt('What are you going to do today?', ' ');
+        const tasks = localStorage.getItem('tasks').split(',');
+        tasks.push(task);
+        localStorage.setItem('tasks', tasks);
         renderListItem(task);
     });
 }
